cd3717428c12372cd78ec032bb1dc17927abf4f9
Lakka <vilakka@apac.corpdir.net>
2020-08-26 17:17:30 +0530

    Modified .py file

--- Role Extraction.py
+++ Role Extraction.py
@@ -1,51 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[13]:
-
-
 import spacy
 from spacy.pipeline import EntityRuler
 from spacy.lang.en import English
-
-
-# In[14]:
 
 
 nlp= English()
 ruler= EntityRuler(nlp)
 nlp.add_pipe(ruler)
 
-
-# In[18]:
-
-
 ### load patterns from disk
 ruler.from_disk('Entity Patterns')
 
+## Load comments below and get the results.
 
-# In[19]:
-
-
-len(ruler.patterns)
-
-
-# In[20]:
-
-
-print(ruler.patterns[:5])
-
-
-# In[30]:
-
-
-doc= nlp(u"Request you to please add 'CS - Supervisor Refund Appr' role for userid VILAKKA")
+doc= nlp(u"Request you to please add 'CS - Supervisor Refund Appr' and 'QOM Administrator' role for userid VILAKKA")
+role_list=[]
 for ent in doc.ents:
-    print(ent.text +'-->'+ ent.label_+'-->'+ ent.ent_id_)
-
-
-# In[ ]:
-
-
+    if ent.label_=='Role':
+        role_list.append(ent.ent_id_)
+        print(ent.text +'-->'+ ent.label_+'-->'+ ent.ent_id_)
+    
 
 
